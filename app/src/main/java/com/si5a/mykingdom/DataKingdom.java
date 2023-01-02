3301b307b548ed491f112f95a18316d0a46b1597
salwa.imleta@gmail.com <112602374+salwafkhra29@users.noreply.github.com>
2023-01-02 21:30:33 +0700

    update
    memasukkan data kingdom seluruh di indonesia pada class DataKingdom

---
+++ app/src/main/java/com/si5a/mykingdom/DataKingdom.java
@@ -0,0 +1,476 @@
+package com.si5a.mykingdom;
+
+import java.util.ArrayList;
+
+public class DataKingdom {
+
+    public static String [] [] data = new String[][] {
+            {"Kerajaan Kutai",
+
+            }
+
+            {"Kerajaan Sribangun",
+
+            }
+
+            {"Kerajaan Wijayapura",
+
+            }
+
+            {"Kerajaan Bakulapura",
+
+            }
+
+            {"Brunei Buddha",
+
+            }
+
+            {"Kerajaan Kuripan",
+
+            }
+
+            {"Kerajaan Negara Dipa",
+
+            }
+
+            {"Kerajaan Negara Daha",
+
+            }
+
+            {"Kerajaan Salakanagara",
+
+            }
+
+            {"Kerajaan Tarumanegara",
+
+            }
+
+            {"Kerajaan Sunda Galuh",
+
+            }
+
+            {"Kerajaan Kalingga",
+
+            }
+
+            {"Kerajaan Kanjuruhan",
+
+            }
+
+            {"Kerajaan Mataram Hindu",
+
+            }
+
+            {"Kerajaan Kahuripan",
+
+            }
+
+            {"Kerajaan Janggala",
+
+            }
+
+            {"Kerajaan Kadiri",
+
+            }
+
+            {"Kerajaan Singasari",
+
+            }
+
+            {"Kerajaan Majapahit",
+
+            }
+
+            {"Kerajaan Malayu Dharmasraya",
+
+            }
+
+            {"Kerajaan Sriwijaya",
+
+            }
+
+            {"Kerajaan Jeumpa",
+
+            }
+
+            {"Kerajaan Peureulek",
+
+            }
+
+            {"Kerajaan Samudera Pasai",
+
+            }
+
+            {"Kesultanan Lamuri",
+
+            }
+
+            {"Kerajaan Pedir",
+
+            }
+
+            {"Kerajaan Daya",
+
+            }
+
+            {"Kerajaan Linge",
+
+            }
+
+            {"Kerajaan Aceh",
+
+            }
+
+            {"Kerajaan Indrapura",
+
+            }
+
+            {"Kerajaan Pagaruyung",
+
+            }
+
+            {"Kerajaan Siguntur",
+
+            }
+
+            {"Kerajaan Sungai Pagu",
+
+            }
+
+            {"Kerajaan Bungo Setangkai",
+
+            }
+
+            {"Kesultanan Jambi",
+
+            }
+
+            {"Kesultanan Serdang",
+
+            }
+
+            {"Kesultanan Asahan",
+
+            }
+
+            {"Kesultanan Deli",
+
+            }
+
+            {"Kesultanan Langkat",
+
+            }
+
+            {"Kesultanan Siak",
+
+            }
+
+            {"Kesultanan Palembang",
+
+            }
+
+            {"Kesultanan Riau Lingga",
+
+            }
+
+            {"Kesultanan Kota Pinang",
+
+            }
+
+            {"Kesultanan Pelalawan",
+
+            }
+
+            {"Kerajaan Indragiri",
+
+            }
+
+            {"Kerajaan Aru",
+
+            }
+
+            {"Kesultanan Barus",
+
+            }
+
+            {"Kerajaan Padang",
+
+            }
+
+            {"Kerajaan Tamiang",
+
+            }
+
+            {"Kerajaan Tulang Bawang",
+
+            }
+
+            {"Kepaksian Sekala Brak",
+
+            }
+
+            {"Kerajaan Dharmasraya",
+
+            }
+
+            {"Kesultanan Cirebon",
+
+            }
+
+            {"Kesultanan Demak",
+
+            }
+
+            {"Kesultanan Banten",
+
+            }
+
+            {"Kesultanan Pajang",
+
+            }
+
+            {"Kesultanan Mataram",
+
+            }
+
+            {"Kesultanan Ngayogyakarta",
+
+            }
+
+            {"Kesunanan Surakarta Hadiningrat",
+
+            }
+
+            {"Kesultanan Ternate",
+
+            }
+
+            {"Kesultanan Tidore",
+
+            }
+
+            {"Kesultanan Jailolo",
+
+            }
+
+            {"Kesultanan Bacan",
+
+            }
+
+            {"Kerajaan Tanah Hitu",
+
+            }
+
+            {"Kerajaan Iha",
+
+            }
+
+            {"Kerajaan Huamual",
+
+            }
+
+            {"Kesultanan Gowa",
+
+            }
+
+            {"Kesultanan Buton",
+
+            }
+
+            {"Kesultanan Bone",
+
+            }
+
+            {"Kesultanan Makassar",
+
+            }
+
+            {"Kerajaan Banggai",
+
+            }
+
+            {"Kerajaan Toli-Toli",
+
+            }
+
+            {"Kerajaan Muna",
+
+            }
+
+            {"Keraaan Buol",
+
+            }
+
+            {"Kerajaan Wajo",
+
+            }
+
+            {"Kedatuan Luwu",
+
+            }
+
+            {"Kerajaan Tallo",
+
+            }
+
+            {"Kerajaan Palu",
+
+            }
+
+            {"Kerajaan Parigi",
+
+            }
+
+            {"Kerajaan Soppeng",
+
+            }
+
+            {"Kerajaan Bungku",
+
+            }
+
+            {"Kerajaan Siang",
+
+            }
+
+            {"Kerajaan Gorontalo",
+
+            }
+
+            {"Kerajaan Mongondow",
+
+            }
+
+            {"Kerajaan Tawaeli",
+
+            }
+
+            {"Kesultanan Bima",
+
+            }
+
+            {"Kesultanan Sumbawa",
+
+            }
+
+            {"Kerajaan Adonara",
+
+            }
+
+            {"Kerajaan Taliwang",
+
+            }
+
+            {"Kerajaan Dompu",
+
+            }
+
+            {"Kerajaan Selaparang",
+
+            }
+
+            {"Kerajaan Lamakera",
+
+            }
+
+            {"Kerajaan Selimbaw",
+
+            }
+
+            {"Kerajaan Sintang",
+
+            }
+
+            {"Kerajaan Mempawah",
+
+            }
+
+            {"Kerajaan Tangjungpura",
+
+            }
+
+            {"Kerajaan Landak",
+
+            }
+
+            {"Kerajaan Kubu",
+
+            }
+
+            {"Kerajaan Bangkalan",
+
+            }
+
+            {"Kerajaan Sanggau",
+
+            }
+
+            {"Kerajaan Tayan",
+
+            }
+
+            {"Kerajaan Kusan",
+
+            }
+
+            {"Kesultanan Pasir",
+
+            }
+
+            {"Kesultanan Banjar",
+
+            }
+
+            {"Kesultanan KotaWaringin",
+
+            }
+
+            {"Kerajaan Pagatan",
+
+            }
+
+            {"Kesultanan Sambas",
+
+            }
+
+            {"Kesultanan Kutai Kartanegara Ing Martadipura",
+
+            }
+
+            {"Kesultanan Berau",
+
+            }
+
+            {"Kesultanan Sambaliung",
+
+            }
+
+            {"Kesultanan Gunung Tabur",
+
+            }
+
+            {"Kesultanan Pontianak",
+
+            }
+
+            {"Kerajaan Tidung",
+
+            }
+
+            {"Kerajaan Tidung Kuno",
+
+            }
+
+            {"Dinasti Tengara",
+
+            }
+
+            {"Kesultanan Bulungan",
+
+            }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/si5a/mykingdom/ModelKingdom.java
@@ -0,0 +1,28 @@
+package com.si5a.mykingdom;
+
+public class ModelKingdom {
+    private String nama;
+    private String tentang;
+    private String foto;
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
